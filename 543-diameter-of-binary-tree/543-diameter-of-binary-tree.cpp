0099ea022d94d9ad799ab90cb222fcd81c7eb6c1
mladenovicluka <104586210+mladenovicluka@users.noreply.github.com>
2022-06-22 18:35:53 +0200

    Time: 55 ms (5.27%), Space: 21.5 MB (6.76%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,17 +11,55 @@
  */
 class Solution {
 public:
-    int maxdepth(TreeNode* root)
-    {
-        if(!root)return 0;
-        return max(maxdepth(root->left),maxdepth(root->right))+1;
-        
-    }
+   
     int diameterOfBinaryTree(TreeNode* root) {
-         if(!root)return 0;
-        int levostablo= maxdepth(root->left);
-        int desnostablo=maxdepth(root->right);
-        int curr=levostablo+desnostablo;
-        return max(curr,max(diameterOfBinaryTree(root->left),diameterOfBinaryTree(root->right)));
+
+       
+        stack <TreeNode* >st;
+        unordered_map<TreeNode*,int> depth;
+        st.push(root);
+        st.push(root);
+        int ans=0;
+         TreeNode* x=root;
+        while(x)
+                {
+                     
+                   st.push(x);
+                   st.push(x);
+                   x=x->left;
+                }
+        while(!st.empty())
+        {
+           x=st.top();
+            st.pop();
+        
+            if(!st.empty()&&x==st.top())
+            {
+                
+                depth[x]=0;
+                x=x->right;
+                while(x)
+                {
+                     
+                   st.push(x);
+                   st.push(x);
+                   x=x->left;
+                }
+                 
+            }
+            else
+            {
+                  
+                depth[x]=max(depth[x->left],depth[x->right])+1;
+                ans=max(ans,depth[x->left]+depth[x->right]);
+                
+                
+                
+            }
+            
+            
+            
+        }
+          return ans;   
     }
 };
