1ab9a043b215e0e548686c8bcda5ba140f233f3d
mladenovicluka <104586210+mladenovicluka@users.noreply.github.com>
2022-07-06 11:21:10 +0200

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int dp[31]={0};
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+            
+        }
+        return dp[n];
+        
+    }
+};
