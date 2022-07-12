23216ce67c68dac3cb92fc125ab955a333e75f66
mladenovicluka <104586210+mladenovicluka@users.noreply.github.com>
2022-07-12 21:22:57 +0200

    Time: 32 ms (32.04%), Space: 17.1 MB (74.38%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> rez;
+        
+        
+        for(int i=0;i<intervals.size();i++)
+        {
+         
+            vector<int> pom;
+            if(rez.size())
+            {
+                
+                
+         
+            if(rez[rez.size()-1][1]>=intervals[i][0])
+            {
+            rez[rez.size()-1][0]=min(rez[rez.size()-1][0],intervals[i][0]);
+            rez[rez.size()-1][1] =max(rez[rez.size()-1][1],intervals[i][1]);
+
+            }
+            else 
+            {
+               rez.push_back({intervals[i][0],intervals[i][1]});
+                
+            }
+               }
+            else
+                rez.push_back({intervals[i][0],intervals[i][1]});
+            
+        }
+        return rez;
+
+        
+    }
+};
